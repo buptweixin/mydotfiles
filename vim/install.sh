93792c8646afbf0f8ab936cd94a9046a587531dc
Wei Xin <weixin@megvii.com>
2019-06-26 07:18:55 +0000

    add no brew branch

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pip install --user autoflake
-pip install --user isort
-pip install -U pycodestyle==2.3.1 --user
-pip install -U neovim --upgrade --user
-pip3 install python-language-server --user
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user autoflake
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user isort
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple --user neovim
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple -U pycodestyle --user
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple python-language-server --user
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
