61251deb9e765b15607365411b82913a2c2fa84d
Wei Xin <weixin@megvii.com>
2019-07-05 14:28:41 +0800

    update

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pip install --user autoflake
-pip install --user isort
-pip install -U pycodestyle==2.3.1 --user
-pip install -U neovim --upgrade --user
+pip3 install --user autoflake
+pip3 install --user isort
+pip3 install -U pycodestyle==2.3.1 --user
+pip3 install -U neovim --upgrade --user
 pip3 install python-language-server --user
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
