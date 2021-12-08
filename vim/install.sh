175335509ce1a0b73c611d156e2c20b67e87e993
User <user@example.com>
2021-12-08 18:12:45 +0800

    update vim and so on

--- vim/install.sh
+++ vim/install.sh
@@ -2,9 +2,9 @@
 
 pip3 install --user autoflake
 pip3 install --user isort
-pip3 install -U pycodestyle==2.3.1 --user
+pip3 install -U pycodestyle --user
 pip3 install -U neovim --upgrade --user
 pip3 install python-language-server --user
 
-# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl 无法下载的话需要配置 proxy ， 方法是新建一个 $HOME/.curlrc 文件， 里面写上 --proxy [protocal]://[url]:[port] 例如 --proxy http:example:8080
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
