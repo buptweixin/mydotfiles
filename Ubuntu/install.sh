4448c868bfa19455d021bd5ef82d956626eb4c8d
Arno.Wei <arno.weixin@shopee.com>
2022-10-24 01:03:43 +0800

    add nvim config

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if test "$(uname)" = "Linux"; then
     if hash apt 2>/dev/null; then
+        sudo apt install software-properties-common -y
+        sudo add-apt-repository ppa:neovim-ppa/stable -y
         sudo apt-get update
-
+        sudo apt install neovim -y
+        sudo apt install npm lua5.3 -y
+        curl -sL install-node.vercel.app/lts | bash # nodejs
+        sudo npm i -g pyright
     fi
 fi
 
