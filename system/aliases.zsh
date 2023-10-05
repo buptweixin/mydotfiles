e0d45e225fc27000d450cfe1e09abac6dbd221ec
Arno.Wei <weixin.bupt.ml@gmail.com>
2023-10-06 00:45:41 +0800

    update zshrc

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,15 +6,15 @@ if [ -x "$(command -v bat)" ]; then
     alias cat="bat"
 fi
 
-alias ll="ls -l"
+# alias ll="ls -l"
 
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+# if $(gls &>/dev/null)
+# then
+#   alias ls="gls -F --color"
+#   alias l="gls -lAh --color"
+#   alias ll="gls -l --color"
+#   alias la='gls -A --color'
+# fi
 
 alias rsync-copy="rsync -avz --progress -h"
 alias rsync-move="rsync -avz --progress -h --remove-source-files"
