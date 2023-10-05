c5d52920e3b1e3bef5a9e414a5799c4210806609
Arno.Wei <arno.weixin@shopee.com>
2023-10-06 01:10:57 +0800

    udpate vim install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -3,4 +3,5 @@ if [ -d $HOME/.config/nvim ]; then
 	echo "Backup old nvim config"
 	mv $HOME/.config/nvim $HOME/.config/nvim.bak
 fi
-ln -sf nvim $HOME/.config/nvim
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ln -sf $SCRIPT_DIR/nvim $HOME/.config/nvim
