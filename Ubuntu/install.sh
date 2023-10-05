5b05ecd67afb94460149ee0ea39295f07ec9ec6f
Arno.Wei <arno.weixin@shopee.com>
2023-10-06 00:45:41 +0800

    update zshrc

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 if test "$(uname)" = "Linux"; then
-    if hash apt 2>/dev/null; then
-        sudo apt install software-properties-common -y
-        sudo add-apt-repository ppa:neovim-ppa/stable -y
-        echo 'deb [trusted=yes] https://apt.fury.io/ascii-image-converter/ /' | sudo tee /etc/apt/sources.list.d/ascii-image-converter.list
-        sudo apt-get update
-        sudo apt install neovim -y
-        sudo apt install npm lua5.3 -y
-        sudo apt install -y ascii-image-converter
-        curl -sL install-node.vercel.app/lts | bash # nodejs
-        sudo npm i -g pyright
-    fi
+	if hash apt 2>/dev/null; then
+		sudo apt install software-properties-common -y
+	fi
 fi
-
