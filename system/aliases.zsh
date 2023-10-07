ce0afde0bd4a36e70635b9ffcec32416d222406b
Arno.Wei <weixin.bupt.ml@gmail.com>
2023-10-07 18:16:00 +0800

    update zshrc

---
+++ system/aliases.zsh
@@ -0,0 +1,8 @@
+if [ -x "$(command -v bat)" ]; then
+    alias cat="bat"
+fi
+
+alias rsync-copy="rsync -avz --progress -h"
+alias rsync-move="rsync -avz --progress -h --remove-source-files"
+alias rsync-update="rsync -avzu --progress -h"
+alias rsync-synchronize="rsync -avzu --delete --progress -h"
