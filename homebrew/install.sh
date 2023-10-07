bbf81e9e150e9c693b40e422860e006fc3162eba
Arno.Wei <weixin.bupt.ml@gmail.com>
2023-10-07 10:44:37 +0800

    update homebrew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,22 +6,15 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
-  # Install homebrew for macos and linux
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  if test "$(uname)" = "Linux"
-  then
-    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  fi
+if test ! $(which brew); then
+	echo "  Installing Homebrew for you."
+	# Install homebrew for macos and linux
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	if test "$(uname)" = "Linux"; then
+		(
+			echo
+			echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+		) >>$HOME/.profile
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	fi
 fi
-
-if test "$(uname)" = "Darwin"
-then
-    brew install TheZoraiz/ascii-image-converter/ascii-image-converter
-    brew install ncdu yarn nvim the_silver_searcher
-fi
-
-exit 0
