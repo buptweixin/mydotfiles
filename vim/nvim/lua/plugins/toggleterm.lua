7025a7908dbf96ef33ca23cd5d44cd9573dc4c61
Arno.Wei <arno.weixin@shopee.com>
2023-11-09 10:02:31 +0800

    enable passthrough mode for tmux

---
+++ vim/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    cmd = { "ToggleTerm", "TermExec" },
+    opts = {
+      highlights = {
+        Normal = { link = "Normal" },
+        NormalNC = { link = "NormalNC" },
+        NormalFloat = { link = "NormalFloat" },
+        FloatBorder = { link = "FloatBorder" },
+        StatusLine = { link = "StatusLine" },
+        StatusLineNC = { link = "StatusLineNC" },
+        WinBar = { link = "WinBar" },
+        WinBarNC = { link = "WinBarNC" },
+      },
+      size = 10,
+      on_create = function()
+        vim.opt.foldcolumn = "0"
+        vim.opt.signcolumn = "no"
+      end,
+      open_mapping = [[<F7>]],
+      shading_factor = 2,
+      direction = "float",
+      float_opts = { border = "rounded" },
+      keys = {
+        { "<leader>ht", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+      },
+    },
+  },
+}
